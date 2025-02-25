e8224d2032395934940a6783c4b5dd73863f6c84
mr-taifur <taifurshishir123@gmail.com>
2025-02-25 12:06:29 +0600

    BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,80 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // The index of the currently selected navigation bar item
+  int _selectedIndex = 0;
+
+  // Method to handle navigation when a bottom navigation item is tapped
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: Text("Drawer Example"), backgroundColor: Colors.orangeAccent),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(color: Colors.orangeAccent),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.person,
-                      size: 80, color: Colors.white), // User icon image
-                  SizedBox(height: 10),
-                  Text("User Name",
-                      style: TextStyle(color: Colors.white, fontSize: 20)),
-                ],
-              ),
+      home: Scaffold(
+        appBar: AppBar(),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              // Display different content based on selected index
+              if (_selectedIndex == 0) ...[
+                // Home Page Content
+                Image.network(
+                  "https://cdn.pixabay.com/photo/2024/01/15/11/36/batman-8510024_960_720.png",
+                ),
+                Image.network(
+                  "https://cdn.pixabay.com/photo/2024/01/15/11/36/batman-8510022_1280.png",
+                ),
+                Image.network(
+                  "https://cdn.pixabay.com/photo/2024/03/26/14/51/ai-generated-8657041_1280.jpg",
+                ),
+              ] else if (_selectedIndex == 1) ...[
+                // Settings Page Content
+                Center(
+                  child: Text('Settings Page'),
+                ),
+              ] else if (_selectedIndex == 2) ...[
+                // About Page Content
+                Center(
+                  child: Text('About Page'),
+                ),
+              ],
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
             ),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text("Home"),
-              onTap: () {
-                Navigator.pop(context); // Close the drawer
-              },
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
             ),
-            ListTile(
-              leading: Icon(Icons.image),
-              title: Text("Images"),
-              onTap: () {
-                Navigator.pop(context); // Close the drawer
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text("Settings"),
-              onTap: () {
-                Navigator.pop(context); // Close the drawer
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.info),
-              title: Text("About"),
-              onTap: () {
-                Navigator.pop(context); // Close the drawer
-              },
+            BottomNavigationBarItem(
+              icon: Icon(Icons.info),
+              label: 'About',
             ),
           ],
         ),
       ),
-      body: Center(
-          child: Text("Swipe from left to open Drawer",
-              style: TextStyle(fontSize: 18))),
     );
   }
 }
